da5953176b417b46408905eea5471c31de26384d
YAEGASHI Takeshi <yaegashi@gmail.com>
2022-06-16 07:50:16 +0000

    Update docker
    - Rebase debian:bullseye-slim
    - Run p4p as a non-root user (p4)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,6 +2,13 @@
 
 set -eu
 
+GOSU="gosu p4"
+
+export P4DIR=/data
+export P4SSLDIR=$P4DIR/ssl
+export P4PCACHE=$P4DIR/cache
+export P4TRUST=$P4DIR/trust
+
 if test -z "$P4TARGET"; then
         echo "E: ENV P4TARGET is not set" >&2
         exit 1
@@ -10,16 +17,17 @@ fi
 set -x
 
 mkdir -p $P4SSLDIR $P4PCACHE
+chown -R p4.p4 $P4DIR
 
 if ! test -r $P4SSLDIR/privatekey.txt -a -r $P4SSLDIR/certificate.txt; then
         chmod 0700 $P4SSLDIR
-        p4p -Gc
+        $GOSU p4p -Gc
 fi
 
 if ! test -r $P4TRUST; then
-        P4PORT=$P4TARGET p4 trust -y
+        $GOSU env P4PORT=$P4TARGET p4 trust -y
 fi
 
-p4p -Gf
+$GOSU p4p -Gf
 
-exec p4p "$@"
+exec $GOSU p4p "$@"
